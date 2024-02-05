5b5be7e2f0e3d0bbbb7e8076236396175985fd19
Alexandra Reaves <nyxalexandra@proton.me>
2024-02-05 16:30:46 -0500

    ggu-mainframe0: move common config into root

--- hosts/ggu-mainframe0/modules/home-manager/alexandra/programs/default.nix
+++ hosts/ggu-mainframe0/modules/home-manager/alexandra/programs/default.nix
@@ -1,12 +1,12 @@
 { pkgs, ... }: {
   imports = [
-    ./alacritty.nix
-    ./git.nix
-    ./helix.nix
-    ./kitty.nix
-    ./nushell.nix
+    ../../../../../modules/home-manager/alacritty.nix
+    ../../../../../modules/home-manager/git.nix
+    ../../../../../modules/home-manager/helix.nix
+    ../../../../../modules/home-manager/kitty.nix
+    ../../../../../modules/home-manager/nushell.nix
+    ../../../../../modules/home-manager/zellij.nix
     ./vscode.nix
-    ./zellij.nix
   ];
   programs.home-manager.enable = true;
 }
